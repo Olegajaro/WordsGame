96c0e8dbc3ca6d91adf8f80208ddbdf93a6b647f
Oleg Fedorov <olegajaro94@gmail.com>
2022-06-18 19:25:35 +0700

    Initial Commit

---
+++ WordsGame/WordsGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordsGameApp.swift
+//  WordsGame
+//
+//  Created by Олег Федоров on 18.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct WordsGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
